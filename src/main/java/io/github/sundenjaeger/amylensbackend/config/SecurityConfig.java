be5ad8b584115a2ba04dd5911aa7c225894109d8
Rentoki <146650376+SundenJaeger@users.noreply.github.com>
2026-05-29 18:24:55 +0800

    Disable auth temporarily

--- src/main/java/io/github/sundenjaeger/amylensbackend/config/SecurityConfig.java
+++ src/main/java/io/github/sundenjaeger/amylensbackend/config/SecurityConfig.java
@@ -1,21 +1,12 @@
 package io.github.sundenjaeger.amylensbackend.config;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,56 +14,55 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
-        return configuration.getAuthenticationManager();
-    }
+//    private final TeamLeadUserDetailsService teamLeadUserDetailsService;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(teamLeadUserDetailsService);
+//        provider.setPasswordEncoder(passwordEncoder);
+//        return provider;
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) {
         return security
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/api/devices/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/devices/auth").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/devices/*/users").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/varieties").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/sessions").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/devices/researchers").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/api-docs/**").permitAll()
-                        .anyRequest().authenticated()
+//                        .requestMatchers(HttpMethod.POST, "/api/devices/register").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/devices/auth").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/devices/*/users").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/varieties").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/sessions").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/devices/researchers").permitAll()
+//                        .requestMatchers("/swagger-ui/**").permitAll()
+//                        .requestMatchers("/swagger-ui.html").permitAll()
+//                        .requestMatchers("/api-docs/**").permitAll()
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .cors(cors -> {
                 })
-                .formLogin(form -> form
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/api/devices", true)
-                        .failureUrl("/api/auth/login?error=true")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessHandler((request, response, authentication) ->
-                                response.setStatus(HttpServletResponse.SC_OK))
-                        .permitAll()
-                )
+//                .formLogin(form -> form
+//                        .loginProcessingUrl("/login")
+//                        .defaultSuccessUrl("/api/devices", true)
+//                        .failureUrl("/login?error=true")
+//                        .permitAll()
+//                )
+//                .logout(logout -> logout
+//                        .logoutUrl("/logout")
+//                        .logoutSuccessHandler((request, response, authentication) ->
+//                                response.setStatus(HttpServletResponse.SC_OK))
+//                        .permitAll()
+//                )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
                 .build();
-    }
-
-    //Temporary rani
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("test"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin);
     }
 }
